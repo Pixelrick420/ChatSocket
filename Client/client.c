153e0f44a8aae1c026511e81900fa6d43ab11477
Harikrishnan R <harikrishnanr0505@gmail.com>
2025-11-01 19:57:37 +0530

    client model

--- Client/client.c
+++ Client/client.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
         exit(1);
     }
 
-    char *ip = "74.125.200.101"; // google ip address for now
+    char *ip = "74.125.200.101";
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_port = htons(80);
@@ -23,5 +24,11 @@ int main()
     {
         printf("Connection Succesfull\n");
     }
+
+    char *message = "GET \\ HTTP/1.1\r\nHost:google.com\r\n\r\n";
+    char buffer[4096];
+    send(socketFD, message, strlen(message), 0);
+    recv(socketFD, buffer, 4096, 0);
+    printf("Response: \n%s\n", buffer);
     return 0;
 }
